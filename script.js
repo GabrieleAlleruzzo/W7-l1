bd98176b6a89da2746973c1e651b48a7f35c72d3
Gabriele Alleruzzo <alleruzzo.gabriele.96@gmail.com>
2025-03-17 16:17:05 +0100

    attempts to confront ages

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-class user {
+class User {
   constructor(_name, _surname, _age, _location) {
     this.name = _name;
     this.surname = _surname;
     this.age = _age;
     this._location = _location;
+  }
+  usersAge(x, y) {
+    if (x.age > y.age) {
+      return x + " è più grnade di" + y;
+    } else if (x.age < y.age) {
+      return y + " è più grnade di" + x;
+    } else {
+      return y + " ha la stessa età di " + x;
+    }
   }
 }
 
@@ -12,3 +21,5 @@ console.log(amico1);
 
 const amico2 = new user("Marko", "Casone", 28, "Ragusa");
 console.log(amico2);
+
+console.log(usersAge(amico1, amico2));
